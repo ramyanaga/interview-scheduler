4497307989cadf3f8bf249dd0c0056f0511cb899
ramyanaga <ramyanaga.99@gmail.com>
2017-07-09 00:40:36 -0700

    Added test js functions to calendar.hbs

---
+++ public/js/calendar.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#dropdown-yes').click(function(event) {
+        console.log('clicked yes');
+    });
+
+    $('#dropdown-maybe').click(function(event) {
+        console.log('clicked maybe');
+    });
+
+    $('#dropdown-no').click(function(event) {
+        console.log('clicked no');
+    });
+});
