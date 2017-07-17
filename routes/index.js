1113e6e8fbdd9298727fc3c8b387a54ca214367a
Ramya Nagarajan <ramyanaga.99@gmail.com>
2017-07-17 12:34:18 -0700

    One panel per student; dropdown button functionality fucked up

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,26 @@ var session = require('express-session');
 
 router.use(bodyParser.json());
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.render('home');
 });
 
-router.get('/calendar', function(req, res) {
+router.get('/calendar', function (req, res) {
     //res.send('Calendar View');
-    res.render('calendar');
+    res.render('calendar', {
+        //students: ['s1', 's2', 's3']
+        student: 'Ramya',
+        students: ['1', '2', '3']
+    }, function (err, data) {
+        if (err) {
+            console.log('CALENDAR VIEW ERROR');
+            console.log(err);
+        }
+        else {
+            res.send(data);
+            console.log("calendar view rendered");
+        }
+    });
 });
 
 module.exports = router;
